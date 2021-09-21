98eb2c15649ff24afdde2c574c7d826905072588
Carlos <carlos.alfonso.montero@gmail.com>
2021-09-20 21:34:19 -0400

    modified cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'example.com'
+     origins 'https://calm-headland-73614.herokuapp.com/'
 
      resource '*',
        headers: :any,

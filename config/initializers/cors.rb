617fca7c6a346eb963c9314e457bac256cceb3e9
Carlos <carlos.alfonso.montero@gmail.com>
2021-09-20 21:36:25 -0400

    cors test localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://calm-headland-73614.herokuapp.com/'
+     origins 'http://localhost:4000/'
 
      resource '*',
        headers: :any,

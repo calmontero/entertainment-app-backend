58d194802ef34764cd5f0ee55f0e5aba42792c56
Carlos <carlos.alfonso.montero@gmail.com>
2021-09-20 22:33:30 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ###################################
-Category.destroy_all
-Genre.destroy_all
-Rating.destroy_all
-Review.destroy_all
-Program.destroy_all
+#Favorite.destroy_all
+#Category.destroy_all
+#Genre.destroy_all
+#Rating.destroy_all
+#Review.destroy_all
+#Program.destroy_all
 #Profile.destroy_all
 #User.destroy_all
 
@@ -23,37 +24,19 @@ Program.destroy_all
 #puts "Seeding profile..."
 ###################################
 
-Category.create([{ name: 'Movies' } , { name: 'TV Show' }])
-puts "Seeding category..."
+#Category.create([{ name: 'Movies' } , { name: 'TV Show' }])
+#puts "Seeding category..."
 ###################################
 
-Genre.create([{ name: 'Adventure' } , { name: 'Scifi' } , { name: "Children & Family Movies" } , { name: "Classic" } , { name: "Comedies" } , { name: "Drama" } , { name: "Romantic" } , { name: "Horror" } , { name: "Sports" } , { name: "Documentaires" }])
-puts "Seeding genre..."
+#Genre.create([{ name: 'Adventure' } , { name: 'Scifi' } , { name: "Children & Family Movies" } , { name: "Classic" } , { name: "Comedies" } , { name: "Drama" } , { name: "Romantic" } , { name: "Horror" } , { name: "Sports" } , { name: "Documentaires" }])
+#puts "Seeding genre..."
 ###################################
 
-Rating.create([{ rating: "G: General Audiences" } , { rating: "PG: Parental Guidance Suggested" }, { rating: "PG-13: Parents Strongly Cautioned" } , { rating: "R: Restricted" }, { rating: "TV-Y: All Children" }, { rating: "TV-G: General Audience" }, { rating: "TV-PG: Parental Guidance Suggested" }, { rating: "TV-MA: Mature Audience Only" }])
-puts "Seeding rating..."
+#Rating.create([{ rating: "G: General Audiences" } , { rating: "PG: Parental Guidance Suggested" }, { rating: "PG-13: Parents Strongly Cautioned" } , { rating: "R: Restricted" }, { rating: "TV-Y: All Children" }, { rating: "TV-G: General Audience" }, { rating: "TV-PG: Parental Guidance Suggested" }, { rating: "TV-MA: Mature Audience Only" }])
+#puts "Seeding rating..."
 ###################################
 
 Program.create([
-  {
-    title: "The Color Purple",
-    year: 1985,
-    length: 154,
-    director: "Steven Spielberg",
-    cast: "Danny Glover,
-    Adolph Caesar,
-    Oprah Winfrey,
-    Margaret Avery,
-    Rae Dawn Chong,
-    Whoopi Goldberg",
-    description: "Whoopi Goldberg brings Alice Walker's Pulitzer Prize-winning feminist novel to life as Celie, a Southern woman who suffered abuse over decades. A project brought to a hesitant Steven Spielberg by producer Quincy Jones, the film marks Spielberg's first female lead.",
-    image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/3071/3071213_so.jpg",
-    imdb_url: "https://www.imdb.com/title/tt0088939/?ref_=nv_sr_srsg_0",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
   {
     title: "The Big Bang Theory",
     year: 2007,
@@ -73,45 +56,7 @@ Program.create([
     image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/The_Big_Bang_Theory_%28Official_Title_Card%29.png/250px-The_Big_Bang_Theory_%28Official_Title_Card%29.png",
     imdb_url: "https://www.imdb.com/title/tt0898266/?ref_=nv_sr_srsg_0",
     category_id: 2,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Frida",
-    year: 2002,
-    length: 123,
-    director: "Julie Taymor",
-    cast: "Salma Hayek,
-    Alfred Molina,
-    Geoffrey Rush,
-    Valeria Golino,
-    Mía Maestro,
-    Roger Rees,
-    Antonio Banderas,
-    Edward Norton",
-    description: "Her portrait, with that thick unibrow and un-waxed upper lip, has become an iconic symbol of feminism. Julie Taymor's biopic takes us behind the canvas to reveal the artist, the activist, the revolutionary. And knowing what we do now about lead actress Selma Hayek's off-screen experience, this film proves an even greater victory.",
-    image_url: "https://m.media-amazon.com/images/M/MV5BYzUxMTU0ZmEtZWE0Ni00NzJlLThhZTUtNDA1ZDZjZDUxYThiXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_.jpg",
-    imdb_url: "https://www.imdb.com/title/tt0120679/?ref_=fn_al_tt_1",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Queen of Katwe",
-    year: 2016,
-    length: 144,
-    director: "Mira Nair",
-    cast: "David Oyelowo,
-    Lupita Nyong'o,
-    Madina Nalwanga,
-    Esteri Tebandeke,
-    Peter Odeke,
-    Sheebah Karungi",
-    description: "Disney has a way of making us feel like pawns in a game of Let's See How Hard We Can Make Them Cry. But that's not the case with Mira Nair's feel-good drama about a Uganda girl's path to chess champ, adapted from an ESPN sports essay. Moms and dads, you want your daughters to grow up to be chess champions.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/12806084/p12806084_v_v8_ar.jpg",
-    imdb_url: "https://www.imdb.com/title/tt4341582/?ref_=fn_al_tt_1",
-    category_id: 1,
-    genre_id: rand(1..10),
+    genre_id: 5,
     rating_id: rand(1..8)
   },
   {
@@ -135,29 +80,7 @@ Program.create([
     image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/6/61/Breaking_Bad_title_card.png/250px-Breaking_Bad_title_card.png",
     imdb_url: "https://www.imdb.com/title/tt0903747/?ref_=nv_sr_srsg_0",
     category_id: 2,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Persepolis",
-    year: 2007,
-    length: 96,
-    director: "Marjane Satrapi",
-    description: "There's a lot at play here: Persepolis is animated like a graphic novel. It's done in black and white. It's about a girl who defies Islamic fundamentalists. It's autobiographical. And it's in French. The result: a brilliant feminist feat from Marjane Satrapi.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/173490/p173490_v_v8_an.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Little Women",
-    year: 1994,
-    length: 118,
-    director: "Gillian Armstrong",
-    description: "Feminism isn't all \"women can do anything men can do\"—it's women can do whatever the hell they want to do. And for the protagonist sisters in Gillian Anderson's adaptation of Louisa May Alcott's novel, that means a writer, a musician, a housewife and a free spirit.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/16296/p16296_v_v8_aj.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
+    genre_id: 5,
     rating_id: rand(1..8)
   },
   {
@@ -180,95 +103,7 @@ Program.create([
     image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Two_and_a_Half_Men-title.png/250px-Two_and_a_Half_Men-title.png",
     imdb_url: "https://www.imdb.com/title/tt0369179/?ref_=nv_sr_srsg_0",
     category_id: 2,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Thelma and Louise",
-    year: 1991,
-    length: 130,
-    director: "Ridley Scott",
-    description: "Susan Sarandon and Gena Davis lock arms—and fates—in this buddy road trip drama with a feminist legacy that runs deeper than the Grand Canyon. Sure, it's directed by Ridley Scott, but the screenplay about a pair of outlaws who are anything but passive comes straight from the pen of Callie Khouri.",
-    image_url: "https://images-na.ssl-images-amazon.com/images/I/517NV58X7WL._SY445_.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Alien",
-    year: 1979,
-    length: 117,  
-    director: "Ridley Scott",
-    description: "Groundbreaking for 1979 science fiction, Sigourney Weaver's Ellen Ripley is one of the genre's most iconic female characters. She's not a damsel—she's a badass final girl and the only survivor to defeat the monster that mutilated and destroyed every other member of the Nostromo crew.",
-    image_url: "https://images-na.ssl-images-amazon.com/images/I/5119C4MWJ0L.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Little Women",
-    year: 2019,
-    length: 135,
-    director: "Greta Gerwig",
-    description: "In the years after the Civil War, Jo March lives in New York and makes her living as a writer, while her sister Amy studies painting in Paris. Amy has a chance encounter with Theodore, a childhood crush who proposed to Jo but was ultimately rejected. Their oldest sibling, Meg, is married to a schoolteacher, while shy sister Beth develops a devastating illness that brings the family back together.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/17008397/p17008397_v_v8_aa.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Mad Max: Fury Road",
-    year: 2015,
-    length: 140,
-    director: "George Miller",
-    description: "Unearthing feminism in a scorched, hell-on-wheels action flick from road warrior franchise man George Miller isn't exactly obvious. But though it's titled after a male character, he is muzzled for much of the film, lending the action to a female imperator and her feminist revolt against the horrors of sexism.",
-    image_url: "https://upload.wikimedia.org/wikipedia/en/6/6e/Mad_Max_Fury_Road.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Mustang",
-    year: 2015,
-    length: 100,
-    director: "Deniz Gamze Ergüven",
-    description: "It's easy to call Deniz Gamze Ergüven's 2015 Oscar nominee a feminist film, since it's about five orphan girls who give oppression and the Turkish patriarchy the middle finger. But it's so much more than that—it's Ergüven's personal comment on what it means to be a woman in Turkey.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/11893143/p11893143_v_v8_ab.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Girlhood",
-    year: 2015,
-    length: 113,
-    director: "Céline Sciamma",
-    description: "Céline Sciamma's Girlhood, a contemporary narrative with a classic plot that isn't too distant from anything in the Jane Austen canon, follows a French teenager's slog through gang life on a quest to self-discovery.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/11240526/p11240526_v_v8_aa.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "Erin Brockovich",
-    year: 2000,
-    length: 113,
-    director: "Steven Soderbergh",
-    description: "Some will say Steven Soderbergh's true-life telling of that time a single mother blew the whistle on PG&E; knowingly poisoning local residents objectifies its lead with scanty costumes. We'd say that's precisely why this film makes our list: because women can wear whatever the hell they want and get sh*t done!",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/24917/p24917_v_v8_aa.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
-    rating_id: rand(1..8)
-  },
-  {
-    title: "The Babadook",
-    year: 2014,
-    length: 95,
-    director: "Jennifer Kent",
-    description: "The portrayal of women in horror films has a ways to go. When it’s not a damsel in a soaked white tank top, it’s a virgin being tortured and exploited. Thankfully, that’s changing. In Jennifer Kent’s dark fairy tale, we have Amelia, a multidimensional character battling monsters and her own inner demons.",
-    image_url: "https://www.gstatic.com/tv/thumb/v22vodart/10644326/p10644326_v_v8_ah.jpg",
-    category_id: 1,
-    genre_id: rand(1..10),
+    genre_id: 5,
     rating_id: rand(1..8)
   }
 ])
@@ -285,7 +120,7 @@ puts "Seeding program..."
       username: Faker::Internet.username,
       comment: Faker::Hipster.sentence,
       rating: rand(1..5),
-      program_id: rand(1..13)
+      program_id: rand(14..16)
     )
   end
 end
